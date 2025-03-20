ca4d992287e8ffffde1b7a8bf450212c93a877f5
cnjszzw <cnjszzw@qq.com>
2025-03-20 17:55:43 +0800

    Feature : 主页文案

--- src/pages/index.js
+++ src/pages/index.js
@@ -86,18 +86,39 @@ export default function Home() {
 
         <div className={styles.content}>
           <section className={`${styles.section} ${styles.bioSection}`}>
-            <h2>简介</h2>
-            <p>嗨👋，我是赵志文，2025年1月硕士毕业于<a href="https://www.xijing.edu.cn/">西京学院</a>电子信息（<a href="https://jsjxy.xijing.edu.cn/">计算机技术方向</a>），在<a href="https://jsjxy.xijing.edu.cn/info/1005/2094.htm">郝东来教授</a>指导下专注于目标检测领域的算法研究。本科阶段就读于<a href="https://www.sdnu.edu.cn/">山东师范大学</a>艺术学（<a href="http://www.cbxy.sdnu.edu.cn/">广播电视编导</a>），兼具艺术与计算机技术的<strong>跨学科背景</strong>。在校期间，也曾经在<a href="https://www.bjljsy.com/">流金科技</a>、<a href="https://baike.baidu.com/item/%E7%BE%8E%E5%9B%A2?fromModule=lemma_search-box">美团</a>、<a href="https://baike.baidu.com/item/%E5%93%94%E5%93%A9%E5%93%94%E5%93%A9/8018053">哔哩哔哩</a>等公司实习，担任过后端开发、测试开发等实习生职位。</p>
-            <p><a href="http://cnjszzw.github.io">INFO.CENTER</a>是我的个人网站，这里不仅是我的博客，也是一个集合文档、工具与技术分享的平台。欢迎探索！</p>
+            <h2 data-en="Biography">简介</h2>
+            <div className={styles.bioContent}>
+              <div className={styles.education}>
+                <p>
+                  Hi👋，我是赵志文，西京学院计算机硕士毕业，本科就读于山东师范大学。
+                </p>
+              </div>
+              
+              <div className={styles.experience}>
+                <p>
+                  实习经历：<a href="https://www.bjljsy.com/">流金科技</a>、
+                  <a href="https://baike.baidu.com/item/%E7%BE%8E%E5%9B%A2">美团</a>、
+                  <a href="https://baike.baidu.com/item/%E5%93%94%E5%93%A9%E5%93%94%E5%93%A9">哔哩哔哩</a>
+                  ，主要从事后端开发与测试开发工作。
+                </p>
+              </div>
+
+              <div className={styles.website}>
+                <p>
+                  欢迎访问我的技术博客 <a href="http://cnjszzw.github.io">INFO.CENTER</a>，
+                  这里有我的文档、工具与技术分享。
+                </p>
+              </div>
+            </div>
           </section>
 
           <section className={`${styles.section} ${styles.miscSection}`}>
-            <h2>杂项</h2>
-            <ul className={styles.miscList}>
-              <li>🌐 该<a href="https://github.com/Cnjszzw/cnjszzw.github.io">个人网站</a>基于<a href="https://docusaurus.io/">Docusaurus</a>构建</li>
-              <li>📺 追剧清单：<a href="https://movie.douban.com/subject/6142597/">《新闻编辑室》</a>、  <a href="https://movie.douban.com/subject/36151693/">《不够善良的我们》</a></li>
-              <li>🎮 游戏收藏：<a href="https://store.steampowered.com/app/646570/">《杀戮尖塔》</a>、<a href="https://store.steampowered.com/app/329050">《鬼泣4》</a></li>
-            </ul>
+            <h2 data-en="Miscellaneous">杂项</h2>
+            <p className={styles.miscText}>
+              📺追剧：<a href="https://movie.douban.com/subject/6142597/">新闻编辑室</a>、<a href="https://movie.douban.com/subject/36151693/">不够善良的我们</a> |
+              🎮游戏：<a href="https://store.steampowered.com/app/646570/">杀戮尖塔</a>、<a href="https://store.steampowered.com/app/329050">鬼泣4</a> |
+              ✨ INFP
+            </p>
           </section>
         </div>
       </main>
